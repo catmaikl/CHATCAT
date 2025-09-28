42150817432af11f3ea967430924ac00b5f3a7e5
catmaikl <kotmajkl88@gmail.com>
2025-09-28 21:20:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ def handle_typing_stop(data):
         'chat_id': data['chat_id']
     }, room=f"chat_{data['chat_id']}", include_self=False)
 
+# Для продакшена на Render
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
